b2492b6cc5cc18546acdf1678d9aafd1b208af0e
Adel <adelgazizova@yandex.ru>
2020-10-22 21:12:59 +0300

    build project

---
+++ docs/precache-manifest.678c03236890c2dfd4539c5405dbc383.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d0e9222e57e1b9b1197baf8c3f1c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2bd361154a1e3685e0",
+    "url": "/static/css/3.5674a87f.chunk.css"
+  },
+  {
+    "revision": "90fa97a73993ffb76c38",
+    "url": "/static/css/main.a8db2a3b.chunk.css"
+  },
+  {
+    "revision": "2c6ce63379beb24192ca",
+    "url": "/static/js/2.6b126feb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b126feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2bd361154a1e3685e0",
+    "url": "/static/js/3.d398d17b.chunk.js"
+  },
+  {
+    "revision": "90fa97a73993ffb76c38",
+    "url": "/static/js/main.eb7f1b92.chunk.js"
+  },
+  {
+    "revision": "32067185b0470d9fdbb6",
+    "url": "/static/js/runtime-main.582765d1.js"
+  },
+  {
+    "revision": "d743e886cb8d3d685c0b174d68c88348",
+    "url": "/static/media/main.d743e886.png"
+  }
+]);
